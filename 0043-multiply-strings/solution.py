22a02c5d46bae0aba072c6f0b698cec8f39f4f6f
Suchita Patil <suchita4004@gmail.com>
2026-09-06 13:26:01 +0530

    Implement string multiplication in solution.py

---
+++ 0043-multiply-strings/solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def multiply(self, num1, num2):
+        if num1 == "0" or num2 == "0":
+            return "0"
+
+        result = [0] * (len(num1) + len(num2))
+
+        for i in range(len(num1) - 1, -1, -1):
+            for j in range(len(num2) - 1, -1, -1):
+                product = int(num1[i]) * int(num2[j])
+
+                pos1 = i + j
+                pos2 = i + j + 1
+
+                total = product + result[pos2]
+
+                result[pos2] = total % 10
+                result[pos1] += total // 10
+
+        answer = ""
+
+        for digit in result:
+            if not (answer == "" and digit == 0):
+                answer += str(digit)
+
+        return answer
