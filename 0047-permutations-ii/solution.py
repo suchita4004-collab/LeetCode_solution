58f33d4ae8a714157a9227f58a1c850988922a05
Suchita Patil <suchita4004@gmail.com>
2026-09-06 13:33:22 +0530

    Add solution for generating unique permutations
    Implement backtracking to generate unique permutations of a list of numbers.

---
+++ 0047-permutations-ii/solution.py
@@ -0,0 +1,28 @@
+class Solution:
+    def permuteUnique(self, nums):
+        result = []
+        nums.sort()
+
+        def backtrack(path, used):
+            if len(path) == len(nums):
+                result.append(path.copy())
+                return
+
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+
+                if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
+                    continue
+
+                used[i] = True
+                path.append(nums[i])
+
+                backtrack(path, used)
+
+                path.pop()
+                used[i] = False
+
+        backtrack([], [False] * len(nums))
+
+        return result
