d5ef6e04aab014bd1201a7a2297b978a4928630c
Suchita Patil <suchita4004@gmail.com>
2026-09-10 20:00:58 +0530

    Add climbStairs method to Solution class
    Implement climbStairs method to calculate ways to climb stairs.

---
+++ 70-climbing-stairs/solution.py
@@ -0,0 +1,16 @@
+```python
+class Solution:
+    def climbStairs(self, n):
+        if n <= 2:
+            return n
+
+        first = 1
+        second = 2
+
+        for _ in range(3, n + 1):
+            third = first + second
+            first = second
+            second = third
+
+        return second
+```
