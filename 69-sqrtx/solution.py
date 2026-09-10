20b78a277d4e553b3dc709d858b5f8e3a5e370d9
Suchita Patil <suchita4004@gmail.com>
2026-09-10 19:59:19 +0530

    Implement binary search for square root calculation

---
+++ 69-sqrtx/solution.py
@@ -0,0 +1,21 @@
+```python
+class Solution:
+    def mySqrt(self, x):
+        if x < 2:
+            return x
+
+        left = 1
+        right = x // 2
+        answer = 0
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if mid <= x // mid:
+                answer = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return answer
+```
