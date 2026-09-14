e7f9fa4a405cfdf8f58157e46b6fc84de92314c7
Suchita Patil <suchita4004@gmail.com>
2026-09-14 19:56:05 +0530

    Add numDecodings method for decoding ways
    Implement a solution to decode ways for a given string.

---
+++ 91-decode-ways/solution.py
@@ -0,0 +1,27 @@
+```python
+class Solution:
+    def numDecodings(self, s):
+        if not s or s[0] == '0':
+            return 0
+
+        prev2 = 1
+        prev1 = 1
+
+        for i in range(1, len(s)):
+            current = 0
+
+            # Decode one digit
+            if s[i] != '0':
+                current += prev1
+
+            # Decode two digits
+            two_digit = int(s[i - 1:i + 1])
+
+            if 10 <= two_digit <= 26:
+                current += prev2
+
+            prev2 = prev1
+            prev1 = current
+
+        return prev1
+```
