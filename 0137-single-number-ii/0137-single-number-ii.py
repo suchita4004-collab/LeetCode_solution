3adfea793169947652d2eb244d3f8746d792ecd1
Suchita Patil <suchita4004@gmail.com>
2026-09-24 13:44:34 +0530

    Time: 33 ms (18.63%), Space: 13.4 MB (70.9%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def singleNumber(self, nums):
+        result = 0
+
+        for i in range(32):
+            count = 0
+
+            for num in nums:
+                if (num >> i) & 1:
+                    count += 1
+
+            if count % 3 != 0:
+                result |= (1 << i)
+
+        # Handle negative numbers
+        if result >= 2**31:
+            result -= 2**32
+
+        return result
